cca4d53570b5531ef72e4d76993fed47180f3014
shaba khan <shabakhan56283@gmail.com>
2024-09-28 14:59:55 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var circle=document.querySelector(".circle")
+var frame=document.querySelector(".frame")
+window.addEventListener("mousemove",function(dets){
+// circle.style.transform=`translate(${dets.clientX}px,${dets.clientY}px)`;
+
+gsap.to(circle,{
+    x:dets.clientX,
+    y:dets.clientY,
+    duration:.3,
+    ease:Expo,
+   
+})
+    
+})
+
+frame.addEventListener("mousemove",function(dets){
+    gsap.to(".circle",{
+        scale:4
+    })
+
+    gsap.to(".frame span",{
+        color:"#fff",
+        duration:.4,
+        y:"-5vw"
+    })
+
+})
+
+frame.addEventListener("mouseleave",function(dets){
+    gsap.to(".circle",{
+        scale:1
+    })
+
+    gsap.to(".frame span",{
+        color:"black",
+        duration:.4,
+        y:"0"
+    })
+})
+
+
+
+
+
+
+
+
+
